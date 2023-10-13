00081d17dd302e967fdc6a668b8cbe383eaa036d
TonnyMpho <tonnympho1@gmail.com>
2023-10-13 01:20:13 -0700

    Fix my code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -2,6 +2,10 @@
 
 class square():
     
+    width = 0
+    height = 0
+
+    
     def __init__(self, *args, **kwargs):
         for key, value in kwargs.items():
             setattr(self, key, value)
@@ -10,8 +14,8 @@ class square():
         """ Area of the square """
         return self.width * self.width
 
-    def permiter_of_my_square(self):
-        return (self.width * 2) + (self.height * 2)
+    def PermiterOfMySquare(self):
+        return (self.width * 2) + (self.width * 2)
 
     def __str__(self):
         return "{}/{}".format(self.width, self.height)
@@ -21,4 +25,4 @@ if __name__ == "__main__":
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.permiter_of_my_square())
+    print(s.PermiterOfMySquare())
